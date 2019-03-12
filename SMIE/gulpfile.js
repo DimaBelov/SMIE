912db693a03b7cfb9f079d126e5f8c0d306553fe
Дмитрий Белов <belov.dmitriy3@wildberries.ru>
2019-03-12 16:55:36 +0300

    #

--- SMIE/gulpfile.js
+++ SMIE/gulpfile.js
@@ -2,10 +2,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var less = require('gulp-less');
-var lessGlobPlugin = require('less-plugin-glob');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
 var minifyCss = require('gulp-csso');
 var minifyJs = require('gulp-uglify');
 var rename = require('gulp-rename');
@@ -19,9 +15,6 @@ var pageStyles = "./wwwroot/css/pages/**/*.css";
 gulp.task("css", function () {
     return gulp.src([pageStyles])
         .pipe(plumber({ errorHandler: handleError }))
-        .pipe(less({ plugins: [lessGlobPlugin] }))
-        .pipe(postcss([autoprefixer({ browsers: ['last 8 versions'] })]))
-        .pipe(gulp.dest(stylesStorage))
         .pipe(minifyCss())
         .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest(stylesStorage));
@@ -35,6 +28,8 @@ var pageScripts = "./wwwroot/js/pages/**/*.js";
 gulp.task("js", function () {
     return gulp.src(pageScripts)
         .pipe(plumber({ errorHandler: handleError }))
+        .pipe(minifyJs())
+        .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest(scriptsStorage));
 });
 
