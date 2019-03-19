8ee68743c4b240ce3bfe1c9a6d556bdfa7410e91
Дмитрий Белов <belov.dmitriy3@wildberries.ru>
2019-03-19 20:03:21 +0300

    personal

---
+++ SMIE/Controllers/PersonalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMIE.Controllers
+{
+    public class PersonalController : Controller
+    {
+        public IActionResult Personal()
+        {
+            return View("Personal");
+        }
+    }
+}
---
+++ SMIE/wwwroot/js/pages/personal/personal.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function() {
+    console.log('personal');
+
+    $('#user-name').text(window.user.username);
+    $('#user-email').text(window.user.email);
+});
